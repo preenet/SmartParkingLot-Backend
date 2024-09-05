36ac0703513e5573e31a44f9a4a2888899cdaee4
thiwakonsakunchao <thiwakon_sakunchao@cmu.ac.th>
2024-09-05 17:16:38 +0700

    Add GET Api for license plate unknown

--- routes/licenseRoutes.js
+++ routes/licenseRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { addLicense, getAllLicensePlates, deleteLicense, editLicense, getLicensePlatesById, addUnknown } = require('../controllers/licenseController');
+const { addLicense, getAllLicensePlates, deleteLicense, editLicense, getLicensePlatesById, addUnknown, getAllUnknown } = require('../controllers/licenseController');
 
 router.post('/addLicense', addLicense);
 router.get('/licensePlates', getAllLicensePlates);
 router.get('/licensePlates/:id', getLicensePlatesById);
 router.delete('/deleteLicense/:id', deleteLicense);
 router.put('/editLicense/:id', editLicense);
-router.post('/addUnknown', addUnknown)
+router.post('/addUnknown', addUnknown);
+router.get('/getAllUnknown', getAllUnknown);
 
 module.exports = router;
